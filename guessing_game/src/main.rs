69ad9315bab910083f95f55d181ae5f8d8475457
mousedoc <sangyun3631@gmail.com>
2023-08-30 12:18:54 +0900

    add guess game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess Number Game");
+    println!("Input your guess");
+
+    // Input 
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).
+        expect("Failed to read line");
+
+    println!("Your guess: {}", guess);
 }
