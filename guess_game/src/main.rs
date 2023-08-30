c96c76b4ae824db8f77b2188293348b186ea0323
mousedoc <sangyun3631@gmail.com>
2023-08-30 17:54:35 +0900

    rand

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,16 +1,22 @@
 use std::io;
 
-fn main() {
-
-    let a: i32 = 10;        // immutable
-    let mut b: i32 = 20;    // mutable 
-
+fn let_example() {
+    let a = 10;         // immutable
+    let mut b = 20;     // mutable
     println!("Before, a was {} and b was {}", a, b);
 
-    // a = 150;             // not avilable
-    b = 250;
+    // a *= 10; not available
+    b *= 10;
 
-    println!("Now, a is {} b is {}", a, b);
+    println!("Now, a is {} and b is {}", a, b);
+}
+
+
+
+fn main() {
+
+    let_example();
+    read_line_example();
 
     println!("Guess Number Game");
     println!("Input your guess");
@@ -19,12 +25,21 @@ fn main() {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).
         expect("Failed to read line");
-
+    
     println!("Your guess: {}", guess);
 
-    
-    println!("Input Example");
-    let result: Result<usize, io::Error> =io::stdin().read_line(&mut guess);
-    result.expect("something wrong");
+}
 
+fn read_line_example() {
+    println!("Input Example");
+
+    let mut input = String::new();asdf
+    let result = io::stdin().read_line(&mut input);
+
+    // if result is 'Err' expect method print msg and stop program
+    result.expect("something wrong");                   
+
+    // r : raw string 
+    // # - # : can conatin "
+    println!(r#"Input is "{}""#, input);
 }
