51ff0d1c6880b8322ddab3c720ef8d8abdeb5513
mousedoc <sangyun3631@gmail.com>
2023-08-31 18:07:48 +0900

    wip

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let a = 5;
+    println!("A is {}", a);
+
+    // Not Available
+    // a = 10;
+    // println!("A is {}", a);
+
+    let mut b = 10;
+    println!("B is {}", b);
+
+    // Available 
+    b = 20;
+    println!("B is {}", b);
+}
