46b421be089acb5dd01e53342a2737388d57a97c
mousedoc <sangyun3631@gmail.com>
2023-09-05 18:08:19 +0900

    qqq

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,38 @@
 fn main() {
+    variable();
+    variable_shadowing();
+    basic_constant();
+}
+
+fn variable() {
     let a = 5;
-    println!("A is {}", a);
+    println!("a is {}", a);
 
     // Not Available
     // a = 10;
     // println!("A is {}", a);
 
     let mut b = 10;
-    println!("B is {}", b);
+    println!("b is {}", b);
 
     // Available 
     b = 20;
-    println!("B is {}", b);
+    println!("b is {}", b);
+}
+
+fn variable_shadowing() {
+    let num = 5;
+    let num = num + 5;
+    let num = num * 20;
+    println!("num is {}", num);
+
+    let length: String = "          ";
+    let length = length.len();
+}
+
+fn basic_constant() {
+    const const_value: i32 = 123;
+
+    println!("const_value is {}", const_value);
+
 }
