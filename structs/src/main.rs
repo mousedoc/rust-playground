cd8c04d1d456730e0c07d32611d9351f8fc10199
mousedoc <sangyun3631@gmail.com>
2023-09-13 16:32:26 +0900

    asdf

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,11 +4,19 @@ struct UserInfo {
     email_address: String,
 }
 
+struct Color(i32, i32, i32);
+struct Vector3(i32, i32, i32);
+
+struct 
+
 fn main() {
-    structs_example();
+    basic_struct();
+    update_struct_expression();
+    struct_with_nameless_field();
+    fieldless_struct();
 }
 
-fn structs_example() {
+fn basic_struct() {
     // create imutable a_user
     let a_user = UserInfo {
         active: true,
@@ -33,8 +41,6 @@ fn structs_example() {
                                                "C@icloud.com".to_owned());
     c_user.active = false;
     print_user_info(&c_user);
-
-
 }
 
 // filed initializer sample
@@ -49,6 +55,40 @@ fn build_user_info(user_name: String, email_address: String) -> UserInfo {
     user
 }
 
+fn update_struct_expression() {
+    let a_user = build_user_info("Gave".to_owned(), 
+                                           "Gave@gmail.com".to_owned());
+
+    // Update expression
+    let b_user = UserInfo {
+        user_name: "Jave".to_owned(),
+
+        // Now, canno use 'a_user' cuz this is '=' operator
+        // and than update expression should be last 
+        ..a_user
+    };
+
+    // cannot 
+    // print_user_info(&a_user);
+    print_user_info(&b_user);
+}
+
+fn struct_with_nameless_field() {
+    let white: Color = Color(255,255,255);
+    let origin = Vector3(0, 0, 0);
+
+    println!("R is {}", white.0);
+    println!("Z is {}", origin.2);
+
+    // cannot, cuz different data type
+    // let black: Color = Color {
+    //     ..origin,
+    // }
+}
+
+fn fieldless_struct() {
+
+}
 
 fn print_user_info(user: &UserInfo) {
     println!("{} {} {}", user.active, 
